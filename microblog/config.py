74b4421b137ca047469d5aaab82199934a45a232
thecodingpie <63698375+the-coding-pie@users.noreply.github.com>
2021-03-21 13:06:39 +0530

    Update config.py

--- microblog/config.py
+++ microblog/config.py
@@ -3,6 +3,6 @@ import os
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
-    SECRET_KEY = 'abcdefg'
+    SECRET_KEY = 'youcantguessit'
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'microblog.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
